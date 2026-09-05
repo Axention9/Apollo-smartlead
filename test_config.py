3bb8543c18a16dd42afbbd7814f96b4203526750
buketkunyeli <buket.mert96@gmail.com>
2026-09-05 22:30:16 +0300

    Remove temporary test files

--- test_config.py
+++
@@ -1,5 +0,0 @@
-# Config sınıfını config.py dosyasından içe aktarır.
-from config import Config
-
-# .env dosyasındaki SECRET_KEY değerinin Config tarınfdan doğru şekilde okunmadığını kontrol eder.
-print(Config.SECRET_KEY)
